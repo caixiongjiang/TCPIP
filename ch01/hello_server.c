d8358b2c7da62b19a1e3dac0788d82a1614b0165
caixiongjiang <1837278011@qq.com>
2022-03-06 20:18:55 +0800

    第一次提交

---
+++ ch01/hello_server.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h> //socket函数头文件
+
+void error_handling(char* message);
+
+int main(int argc, char* argv[])
+{
+    int serv_sock; 	
+}
